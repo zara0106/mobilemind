626d4a9f7a1def43bce2c31f9c72d637a31f3778
ROBUSTE Manohisafidy <safidy@evalucar.fr>
2024-08-18 04:03:26 +0300

    union
    Signed-off-by: ROBUSTE Manohisafidy <safidy@evalucar.fr>

--- src/components/sidebar/menu-item/sub-menu-item.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function SubMenuItem(props) {
-  const { text } = props;
-
-  return <div className="menu-item__sub-item">{text}</div>;
-}
-
-export default SubMenuItem;
